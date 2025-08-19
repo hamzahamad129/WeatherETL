3f606dbe9408cd4db6944287a76651f949e32513
hamzahamad129 <hamzahhammad305@gmail.com>
2025-08-19 16:12:22 +0500

    Adding one more file of python and making some changes

--- etl sql script.sql
+++ etl sql script.sql
@@ -1,13 +1,13 @@
-create database weatherDB;
+create database weatherDB;                     -- Creation of DataBase
 use weatherDB;
-create table weather_hourly (
+create table weather_hourly (                  -- Creation of table
      `date` date,
      `hour` time,
      temperature float
 );
 select * from weather_hourly;
 
-select `Date`,avg(Temperature) from weather_hourly
+select `Date`,avg(Temperature) from weather_hourly          --Data analysis of the Table
 group by `Date`;
 
 select `Time`,avg(Temperature) from weather_hourly
